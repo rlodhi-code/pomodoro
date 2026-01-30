b086159ae3bdcee3634d3cce956c88077d85bcff
rizwanlodhi <rizwan.lodhi@gmail.com>
2026-01-29 19:46:26 -0600

    Added timer code using function count_down(count).

--- main.py
+++ main.py
@@ -15,17 +15,33 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
+def count_down(count):
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodoro (tomato)")
 window.config(padx=100, pady=50, bg=YELLOW)
+
+count_down(5)
+
+title_label = Label(window, text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(row=0, column=1)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row=1, column=1)
 
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(row=2, column=2)
+
+check_mark = Label(text="✔️", fg=GREEN, bg=YELLOW)
+check_mark.grid(row=3, column=1)
 
 window.mainloop()
